24c7cfa8439be86abb8054983756b262e1072608
Anna Belokopytova <belo.anna.a91@gmail.com>
2026-02-02 03:05:58 +0300

    files added

---
+++ src/js/__tests__/user.test.js
@@ -0,0 +1,23 @@
+import { sortHeroes } from '../user';
+
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+
+test('сортировка по убыванию здоровья', () => {
+  const sorted = sortHeroes(heroes);
+
+  expect(sorted).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+
+  expect(heroes).toEqual([
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ]);
+});
